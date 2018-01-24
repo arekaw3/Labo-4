fdab4716c86aac100ddc16c02bec27ff27709690
arekaw3 <32580032+arekaw3@users.noreply.github.com>
2018-01-24 15:45:50 +0100

    Add files via upload

---
+++ Ćwiczenia 11/Zadanie 1/Zadanie 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void parzysta(int a){
+	if(a&1==1)
+	printf("nieparzysta");
+	else
+	printf("parzysta");
+}
+
+int main(){
+	int a;
+	scanf("%d",&a);
+	parzysta(a);
+	return 0;
+}
